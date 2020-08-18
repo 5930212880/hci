51b0cf06a42c3715af1d7d5d8f1fc9a348e04925
USER <EMAIL>
2020-08-18 20:14:10 +0700

    initial commit

---
+++ lecture.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		
+		
+		<title>HCI</title>
+		
+		
+		<!--=====================================================
+			CSS Stylesheets
+		=====================================================-->
+		<link rel='stylesheet' type='text/css' href='bootstrap/css/bootstrap.min.css' >
+		<link rel='stylesheet' type='text/css' href='css/linea.css' >
+		<link rel='stylesheet' type='text/css' href='css/ionicons.min.css' >
+		<link rel='stylesheet' type='text/css' href='css/owl.carousel.css' >
+		<link rel='stylesheet' type='text/css' href='css/magnific-popup.css' >
+		<link rel='stylesheet' type='text/css' href='css/style.css' >
+		
+	</head>
+	<body>
+		
+		
+		<!--=====================================================
+			Preloader
+		=====================================================-->
+		<div id='preloader' >
+			<div class='loader' ></div>
+			<div class='left' ></div>
+			<div class='right' ></div>
+		</div>
+		
+		
+		<div class='main-content' >
+			
+			<!--=====================================================
+				Page Borders
+			=====================================================-->
+			<div class='page-border border-left' ></div>
+			<div class='page-border border-right' ></div>
+			<div class='page-border border-top' ></div>
+			<div class='page-border border-bottom' ></div>
+			
+			
+			
+			<!--=====================================================
+				Menu Button
+			=====================================================-->
+			<a href='index.php' class='menu-btn' >
+				<span class='lines' >
+					<span class='l1' ></span>
+					<span class='l2' ></span>
+					<span class='l3' ></span>
+				</span>
+			</a>
+			
+			
+			<!--=====================================================
+				Menu
+			=====================================================-->
+			<div class='menu' >
+				<div class='inner' >
+					<ul class='menu-items' >
+						
+						<li>
+							<a href='index.php' class='section-toggle' data-section='intro' >
+								Home
+							</a>
+						</li>
+						
+						<li>
+							<a href='#resume' class='section-toggle' data-section='resume' >
+								Group
+							</a>
+						</li>
+						
+						<li>
+							<a href='#portfolio' class='section-toggle' data-section='portfolio' >
+								Portfolio
+							</a>
+						</li>
+						
+						
+						<li>
+							<a href='#contact' class='section-toggle' data-section='contact' >
+								Assignment
+							</a>
+						</li>
+						
+						
+					</ul>
+				</div>
+			</div>
+            
+			
+		<div class='animation-block' ></div>
+			
+			
+			<!--=====================================================
+				Sections
+			=====================================================-->
+			<div class='sections' >
+				
+				<!--=====================================================
+					About Section
+				=====================================================-->
+				<section id='about' class='section about-section border-d' >
+					
+					<div class='section-block about-block' >
+						<div class='container-fluid' >
+							
+							<div class='section-header' >
+								<h2>
+									<strong class='color' >HUMAN-COMPUTER INTERACTION<br>
+									Slide Lecture</strong>
+								</h2>
+							</div>
+							
+							<div class='row' style='margin-top: -50px;' >
+								
+								<div class='col-md-4' >
+									
+									<ul class='info-list' >
+					
+										<li>
+											<strong><h3>TOPIC</h3></strong>
+										</li>
+
+										<li>
+										
+										</li>
+
+										<li>
+										
+										</li>
+									</ul>
+									
+								</div>
+                            </div>
+                        </div>
+                    </div>    
+
+			
+                </section>
+            </div>
+        </div>
+
+		<!--=====================================================
+			JavaScript Files
+		=====================================================-->
+		<script src='js/jquery.min.js' ></script>
+		<script src='js/jquery.shuffle.min.js' ></script>
+		<script src='js/owl.carousel.min.js' ></script>
+		<script src='js/jquery.magnific-popup.min.js' ></script>
+		<script src='js/validator.min.js' ></script>
+		<script src='js/script.js' ></script>
+		
+	</body>
+</html>
